aaa796e1e72ccf3db700a214c292147c0aad48ee
Noah Schefer <nbschefer@gmail.com>
2019-11-04 14:58:46 -0500

    models created and associated

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,4 @@
-const db = require('./database');
+const db = require('./db');
+
 
 module.exports = db;
--- server/db/models/Student.js
+++ server/db/models/Student.js
@@ -1,6 +1,28 @@
 const db = require('../index');
 const Sequelize = require('sequelize');
 
-const Campus = db.define('campus', {
-
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  major: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
 })
+
+module.exports = Student;
